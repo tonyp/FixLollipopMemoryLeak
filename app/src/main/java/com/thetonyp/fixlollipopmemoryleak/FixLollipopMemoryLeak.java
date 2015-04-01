4b3bbb886d8315fb1f9eaf60e86ae1fb410a47dd
tonyp <tonyptonyp@gmail.com>
2015-04-01 02:27:47 +0200

    Further improve string comparison
    This could avoid a rare condition NPE as "android" is never null and
    thus equals() will always be valid.

--- app/src/main/java/com/thetonyp/fixlollipopmemoryleak/FixLollipopMemoryLeak.java
+++ app/src/main/java/com/thetonyp/fixlollipopmemoryleak/FixLollipopMemoryLeak.java
@@ -26,7 +26,7 @@ public class FixLollipopMemoryLeak implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("android") || !lpparam.processName.equals("android"))
+        if (!"android".equals(lpparam.packageName) || !"android".equals(lpparam.processName))
             return;
 
         if (Build.VERSION.SDK_INT != Build.VERSION_CODES.LOLLIPOP) {
